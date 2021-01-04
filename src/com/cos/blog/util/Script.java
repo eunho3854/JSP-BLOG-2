678bf16dedac4d596ba4254f18b37afd0212020c
eunho <eunho3854@gmail.com>
2021-01-04 11:38:33 +0900

    JSP 블로그 만들기 (로그인, 회원가입 구조 만들기)

---
+++ src/com/cos/blog/util/Script.java
@@ -0,0 +1,18 @@
+package com.cos.blog.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class Script {
+	public static void back(HttpServletResponse resp,String msg) throws IOException {
+		PrintWriter out=resp.getWriter();
+		out.println("<script>");
+		out.println("alert(`"+msg+"`);");
+		out.println("history.back();");
+		out.println("</script>");
+		out.flush();
+	}
+
+}
